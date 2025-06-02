5cc1bd7551e322f73a926041e0f71718d3d22b83
anshori <m.anshory@gmail.com>
2025-06-02 18:24:28 +0700

    Add fontawesome css library to fixing search icon on map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -3,6 +3,7 @@
 @section('style')
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   <style>
     #map {
       margin-top: 56px;
